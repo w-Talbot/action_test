d9f362629d2ee3e3ed2544a7b2e9c8cfdd313e0e
w-talbot <wesstalbot@gmail.com>
2022-07-30 18:35:25 +0100

    practice only: first commit

---
+++ main.py
@@ -0,0 +1,21 @@
+
+def check_diff( ga, gb ):
+    # Needs to loop through each piece of several files, check them as groups
+    for i in range(3):
+        for i in instruments_group_a:
+            print("hi")
+
+
+instruments_group_a = ["first_name", "last_name", "dob", "sex", "weight"]
+instruments_group_b = ["first_name", "last_name", "dob", "sex", "weight"]
+
+survey_queue_a = {"contact_details":1 , "screening_log":2 , "consent":3, "randomisation":4, "randomisation_outcome":5}
+survey_queue_b = {"contact_details":1 , "screening_log":2 , "consent":3, "randomisation":4, "randomisation_outcome":5}
+
+crfs_a = ["contact_details", "screening_log", "consent",  "randomisation", "randomisation_outcome" ]
+crfs_b = ["contact_details", "screening_log", "consent",  "randomisation", "randomisation_outcome" ]
+
+group_a = [instruments_group_a, survey_queue_a, crfs_a]
+group_b = [instruments_group_b, survey_queue_b, crfs_b]
+
+check_diff(group_a, group_a)
